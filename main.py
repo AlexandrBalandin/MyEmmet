8b680902249bb6f495f682cd4f2c9cacd7dd1a12
AlexandrBalandin <avuabhazu74rus@gmail.com>
2020-02-05 23:08:57 +0500

    Добавил комментарии к разработке

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ with open("snippets/html.json", "r") as read_file:
     html = json.load(read_file)
 
 
-print(type(html))
+print(html)#разбить ключи в словаре
+
 inputString = input()
 
-parsedString = inputString.split('+')
+parsedString = inputString.split('+')#заменить на нормальный парсер операций
 
 
 def WrapInTag(text):
     return "<" + text + ">" + "<\\" + text + ">"
 
-
+#если нет строк в словаер, просто оборачивать в теги
 def OpenString():
     for word in parsedString:
         print(WrapInTag(html[word]))
+
+
 OpenString()
